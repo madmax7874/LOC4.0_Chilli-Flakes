f6568176a363c8c53d981422798f1ef0d00d59e7
madmax7874 <rushabhshah2001@gmail.com>
2022-03-12 15:36:45 +0530

    server schema creation

--- server/models/User.js
+++ server/models/User.js
@@ -18,8 +18,39 @@ const UserSchema = new mongoose.Schema({
     minlength: 6,
     select: false,
   },
+  role: {
+    type: String,
+    required: [true, "Please choose a role"],
+  }
 });
 
-const User = mongoose.model("User", UserSchema);
+const ItemSchema = new mongoose.Schema({
+  name: {type: String, required: [true, "Please provide name"]},
+  quantity: {type: String},
+});
 
-module.exports = User;
+const OrderSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: "Users",
+    },
+    orderDetails: [
+      {
+        items: { type: mongoose.Types.ObjectId, ref: "Items" },
+        quantity: { type: Number, required: true },
+        paymode: { type: String, required: true },
+        status: { type: String, default: "Placed" },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const ItemModel = mongoose.model("Items", ItemSchema);
+const OrderModel = mongoose.model("Orders", OrderSchema);
+const User = mongoose.model("Users", UserSchema);
+
+module.exports = User,OrderModel,ItemModel;
