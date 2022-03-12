8e172ad2849e165f260533c41f9ba75fe43a612d
madmax7874 <rushabhshah2001@gmail.com>
2022-03-12 13:02:08 +0530

    basics added

---
+++ server/routes/private.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const { protect } = require("../middleware/auth");
+const jwt = require("jsonwebtoken");
+const ErrorResponse = require("../utils/errorResponse");
+const User = require("../models/User");
+
+router.route("/").get(protect , async(req,res) => {
+  res.status(200).send(true);
+});
+
+module.exports = router;
