9f286f7b3cb43dcbbf92a8d4b616104b5af68a9c
ParasGada <parasgada0@gmail.com>
2022-03-12 19:45:22 +0530

    .

--- server/routes/private.js
+++ server/routes/private.js
@@ -3,20 +3,20 @@ const router = express.Router();
 const { protect } = require("../middleware/auth");
 const jwt = require("jsonwebtoken");
 const ErrorResponse = require("../utils/errorResponse");
-const {User,ItemModel} = require("../models/User");
+const { User, ItemModel } = require("../models/User");
 
-router.route("/").get(protect , async(req,res) => {
+router.route("/").get(protect, async (req, res) => {
   res.status(200).send(true);
 });
 
-router.route("/store").get(protect, async (req, res, next) => { 
-  try{
+router.route("/store").get(protect, async (req, res, next) => {
+  try {
     const items = await ItemModel.find();
-    console.log(items)
+    console.log(items);
     res.status(200).send(items);
-  }catch(err){
-    next(err)
+  } catch (err) {
+    next(err);
   }
-})
+});
 
 module.exports = router;
